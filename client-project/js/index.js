ff8c5c4e3f5838faa63f0a5f8c3203f4656e76c1
Shuaibu78 <freezzemx@gmail.com>
2019-02-01 08:44:25 +0100

    the nav and jumbootron update

--- client-project/js/index.js
+++ client-project/js/index.js
@@ -1,1 +1,9 @@
 // JS goes here
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
