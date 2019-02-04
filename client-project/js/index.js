e5a87aa0298ecd68ca367324c2236942dcd90101
Abdulhakeem <ayoazeez26@gmail.com>
2019-02-04 17:11:04 +0100

    created mobile version of the home page and created the services page

--- client-project/js/index.js
+++ client-project/js/index.js
@@ -1,1 +1,20 @@
-// JS goes here
+document.getElementById("defaultOpen").click();
+
+function openDescription(evt, descName) {
+    var i, navigator, navigator_components;
+
+    navigator = document.getElementsByClassName("navigator-components");
+    for( i = 0; i < navigator.length; i++) {
+        navigator[i].style.display = "none";
+    }
+
+    navigator_components = document.getElementsByClassName("navigator-components");
+        for (i = 0; i < navigator_components.length; i++) {
+            navigator_components[i].className = navigator_components[i].className.replace(" active", "");
+    }
+
+    document.getElementById(descName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    console.log(evt.currentTarget.className);
+}
