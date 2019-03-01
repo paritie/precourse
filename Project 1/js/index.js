92e990fe7264c60730eef9e67c4ca80bc99bbbf9
Alemoh Rapheal <raphealenike@gmail.com>
2019-03-01 08:03:15 -0800

    Paritie Client Project done by AlemsBaja

--- Project 1/js/index.js
+++ Project 1/js/index.js
@@ -11,15 +11,24 @@ function closeSideMenu(){
 }
 
 
-// function myFunction(imgs) {
-//     // Get the expanded image
-//     var expandImg = document.getElementById("expandedImg");
-//     // Get the image text
-//     var imgText = document.getElementById("imgtext");
-//     // Use the same src in the expanded image as the image being clicked on from the grid
-//     expandImg.src = imgs.src;
-//     // Use the value of the alt attribute of the clickable image as text inside the expanded image
-//     imgText.innerHTML = imgs.alt;
-//     // Show the container element (hidden with CSS)
-//     expandImg.parentElement.style.display = "block";
-//   }
+function openImage(evt, imageName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(imageName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
