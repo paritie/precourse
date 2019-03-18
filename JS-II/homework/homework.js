579995930baca1a0a8606aa39ace8e8fad0b3ecc
torisamson <omavikforfame@gmail.com>
2019-03-18 12:02:14 +0100

    JS-II homework added

--- JS-II/homework/homework.js
+++ JS-II/homework/homework.js
@@ -3,6 +3,13 @@
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+ const x = 10;
+ const y = 5;
+  if (x > y) {
+    console.log(10);
+  } else {
+    console.log(5);
+  }
 }
 
 function greeting(language) {
@@ -11,16 +18,41 @@ function greeting(language) {
   // language: 'Mandarin' -> 'Ni Hao!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  const language = German;
+  const language = Mandarin;
+  const language = Spanish;
+
+  if (German || Mandarin || Spanish) {
+    console.log('Hola');
+  } else {
+    console.log('Hello');
+  }
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  const num = 10;
+  const num = 5;
+
+  if ('num' === 10) {
+    console.log('true');
+  } else if ('num' === 5) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
+  const num = 35.5;
+  if ('num' < 50) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
 }
 
 function isInteger(num) {
@@ -30,6 +62,12 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  const num = 0.5;
+  if (integer) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
 }
 
 function fizzBuzz(num) {
@@ -37,6 +75,16 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  const num = 10;
+  if (10 / 3) {
+    console.log('fizz');
+  } else if (10 / 5) {
+    console.log('buzz');
+  } else if (10 / 3 && 10 / 5) {
+    console.log('fizzbuzz');
+  } else {
+    console.log('num');
+  }
 }
 
 function isPrime(num) {
@@ -45,6 +93,12 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  const num = 100;
+  if (prime) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
 }
 
 
