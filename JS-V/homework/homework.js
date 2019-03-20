cb0699fcab70238c2947ea505bad0c380d0c33d3
torisamson <omavikforfame@gmail.com>
2019-03-20 15:01:37 +0100

    JS-V added

--- JS-V/homework/homework.js
+++ JS-V/homework/homework.js
@@ -7,11 +7,32 @@ function createUserClass() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the class
-}
+  const user = new User(
+  {
+    username: 'SunJieMing', 
+    name: 'Ben', 
+    email: 'ben@lambdaschool.com', 
+    password: 'LS Rocks!'
+    sayHi : function() {
+      return 'Hello, my name is Ben';
+
+    }
+
+     }
+
+     );
+  return user;
+  }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+  const user = new User(
+  {
+    sayHi : function() {
+      return 'Hello World';
+    }
+  });
 }
 
 function addReverseString() {
@@ -19,6 +40,14 @@ function addReverseString() {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+  const str = new 'Hello' (
+  {
+    reverse : function() {
+      this.reverse = 'olleH';
+    }
+
+  });
+  return reverse;
 }
 
 // Do not modify code below this line.
